08561143c58dd109f69ab35ce1a80c8469a47725
ravina01 <91215561+ravina01@users.noreply.github.com>
2021-10-27 22:58:51 -0400

    updated apriltag file
    if getting indentation error :  autopep8 -i my_file.py
    pip3 install autopep8

--- mobile_robotics/src/scripts/aprilTagFinal.py
+++ mobile_robotics/src/scripts/aprilTagFinal.py
@@ -6,81 +6,53 @@ import tf
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Twist
 from std_msgs.msg import Float64
+from apriltag_ros.msg import AprilTagDetectionArray
 
 stop_flag = 0
+def get_init_position():
+    data_odom = None
+    while data_odom is None:
+        try:
+            data_odom = rospy.wait_for_message("/odom", Odometry, timeout=1)
+        except:
+            rospy.loginfo(
+                "Current odom not ready yet, retrying for setting up init pose")
+
+    current_pose = Point()
+    current_pose.x = data_odom.pose.pose.position.x
+    current_pose.y = data_odom.pose.pose.position.y
+    current_pose.z = data_odom.pose.pose.position.z
+    return current_pose
 
 
-class MovementDetector(object):
-    def __init__(self):
-        self._mved_distance = Float64()
-        self._mved_distance.data = 0.0
-        self.get_init_position()
+def callback(data):
 
-        self.distance_moved_pub = rospy.Publisher(
-            '/moved_distance', Float64, queue_size=1)
-        rospy.Subscriber("/odom", Odometry, self.odom_callback)
+    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    speed = Twist()
+    speed.linear.x = 0.1760
 
-    def get_init_position(self):
-        data_odom = None
-        while data_odom is None:
-            try:
-                data_odom = rospy.wait_for_message(
-                    "/odom", Odometry, timeout=1)
-            except:
-                rospy.loginfo(
-                    "Current odom not ready yet, retrying for setting up init pose")
+    t = 5
+    stop_dist = trans[2] - 0.12
+    while not rospy.is_shutdown():
+        pub.publish(speed)
+        current_pose = get_init_position()
 
-        self._current_position = Point()
-        self._current_position.x = data_odom.pose.pose.position.x
-        self._current_position.y = data_odom.pose.pose.position.y
-        self._current_position.z = data_odom.pose.pose.position.z
-
-    def odom_callback(self, msg):
-        NewPosition = msg.pose.pose.position
-        self._mved_distance.data += self.calculate_distance(
-            NewPosition, self._current_position)
-        self.updatecurrent_positin(NewPosition)
-        if self._mved_distance.data < 0.000001:
-            aux = Float64()
-            aux.data = 0.0
-            self.distance_moved_pub.publish(aux)
-        else:
-            self.distance_moved_pub.publish(self._mved_distance)
-
-    def updatecurrent_positin(self, new_position):
-        self._current_position.x = new_position.x
-        self._current_position.y = new_position.y
-        self._current_position.z = new_position.z
-
-    def calculate_distance(self, new_position, old_position):
-        x2 = new_position.x
-        x1 = old_position.x
-        y2 = new_position.y
-        y1 = old_position.y
-        dist = math.hypot(x2 - x1, y2 - y1)
-        return dist
+        if current_pose.x > stop_dist:
+            speed.linear.x = 0
+            pub.publish(speed)
+            rospy.spin()
 
 
 if __name__ == '__main__':
-    rospy.init_node('aprilTag_detector_node', anonymous=True)
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-    speed = Twist()
-    speed.linear.x = 0.06
-    movement_obj = MovementDetector()
-    
+    rospy.init_node('Node', anonymous=True)
     listener = tf.TransformListener()
     
-    while not rospy.is_shutdown():
+    while(True):
         if(stop_flag == 0):
             try:
                 (trans, rot) = listener.lookupTransform('/tag_0', '/base_footprint', rospy.Time(0))
-                print(trans)
                 stop_flag = 1
             except (tf.LookupException, tf.ConnectivityException):
                 continue
-
-        stop_dist = trans[2] - 0.12
-        pub.publish(speed)
-        if movement_obj._current_position.x > stop_dist:
-            speed.linear.x = 0
-            pub.publish(speed)
+        sub = rospy.Subscriber(
+            "/tag_detections", AprilTagDetectionArray, callback)
